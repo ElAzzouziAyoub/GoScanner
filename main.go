461bb894718c765193bc42a541b810c61ef5773c
ElAzzAyoub <Ayoub.ELAZZOUZI@UM6P.ma>
2026-01-16 21:20:59 +0100

    Adding more details to results

--- main.go
+++ main.go
@@ -5,12 +5,45 @@ import (
 	"net"
 	"sync"
 	"flag"
+	"os"
+	"log"
+	"encoding/json"
 )
 
 var wg sync.WaitGroup
 
+type PortService struct {
+    Port        int    `json:"port"`
+    IsTCP       bool   `json:"is_tcp"`
+    IsUDP       bool   `json:"is_udp"`
+    Description string `json:"description"`
+    IsOfficial  bool   `json:"is_official"`
+}
+
+
 
 func main() {
+
+	data, err := os.ReadFile("ports.json")
+	if err != nil {
+    log.Fatal(err)
+	}
+
+	var services []PortService
+
+	err = json.Unmarshal(data, &services)
+	if err != nil {
+    log.Fatal(err)
+	}
+
+	serviceMap := make(map[int]PortService)
+
+	for _, svc := range services {
+    serviceMap[svc.Port] = svc
+	}
+
+
+
 
 	target := flag.String("target", "", "Target host")
 	StartPort := flag.Int("sp", 80, "Starting Port to scan")
@@ -41,8 +74,17 @@ func main() {
 		go func(a int) {
 			defer wg.Done()
 			_, err := net.Dial(*Type,fmt.Sprintf("%s:%d",*target,a))
+			port := serviceMap[a]
 			if err == nil {
 				fmt.Println("Port Opened : ",a)
+				fmt.Println("Description :",port.Description)
+				if port.IsTCP {
+					fmt.Println("Type : TCP")
+				}else{
+					fmt.Println("Type : UDP")
+				}
+				fmt.Println("IsOfficial :",port.IsOfficial)
+				fmt.Println("\n")
 			}
 
 		}(i)
